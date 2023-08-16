6c2a1ff19bd773203a9668228f5780091179dfbe
Brandon Jones <cerealfordinnerx@gmail.com>
2023-08-15 21:48:27 -0400

    Get latest article

--- AdeptusBoticus/Program.cs
+++ AdeptusBoticus/Program.cs
@@ -1,4 +1,4 @@
 ﻿using System.Xml.Linq;
 using AdeptusBoticus;
 
-var feedDoc = FeedService.GetFeedAsync("warhammer-community.com/feed/");
+var feedService = new FeedService("https://www.warhammer-community.com/feed/");
--- AdeptusBoticus/Services/FeedService.cs
+++ AdeptusBoticus/Services/FeedService.cs
@@ -5,11 +5,31 @@ namespace AdeptusBoticus;
 
 public class FeedService
 {
-    public static async Task<XDocument> GetFeedAsync(string feedUrl)
+    public string FeedUrl { get; set; }
+    public FeedService(string feedUrl)
+    {
+        FeedUrl = feedUrl;
+    }
+    public async Task<XDocument> GetFeedAsync()
     {
         using var httpClient = new HttpClient();
-        string xmlContent = await httpClient.GetStringAsync(feedUrl);
+        string xmlContent = await httpClient.GetStringAsync(FeedUrl);
         return XDocument.Load(xmlContent);
+    }
+
+    public Article GetLatestArticle(XDocument xmlDoc)
+    {
+        var articles = xmlDoc.Descendants("item");
+        var latestArticleElement = articles.FirstOrDefault();
+
+        if (latestArticleElement != null)
+        {
+            Article latestArticle = new Article
+            {
+                Title = latestArticleElement.Element("title")?.Value,
+                Link = latestArticleElement.Element("link")?.Value,
+            };
+        }
     }
 
 }
