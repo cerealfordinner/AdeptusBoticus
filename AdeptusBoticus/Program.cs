3b3280c1c61a6980cf7e54001225e0f01c9a089e
Brandon Jones <cerealfordinnerx@gmail.com>
2023-08-19 18:13:23 -0400

    Add helper for the publication date

--- AdeptusBoticus/Program.cs
+++ AdeptusBoticus/Program.cs
@@ -2,3 +2,7 @@
 using AdeptusBoticus;
 
 var feedService = new FeedService("https://www.warhammer-community.com/feed/");
+
+Task<XDocument> xmlDocTask = feedService.GetFeedAsync();
+var xmlDoc = xmlDocTask.Result;
+Article latestArticle = feedService.GetLatestWarhammerArticle(xmlDoc);
--- AdeptusBoticus/Services/FeedService.cs
+++ AdeptusBoticus/Services/FeedService.cs
@@ -14,22 +14,37 @@ public class FeedService
     {
         using var httpClient = new HttpClient();
         string xmlContent = await httpClient.GetStringAsync(FeedUrl);
-        return XDocument.Load(xmlContent);
+        XDocument xmlDoc = XDocument.Parse(xmlContent);
+        return xmlDoc;
     }
 
-    public Article GetLatestArticle(XDocument xmlDoc)
+    public Article GetLatestWarhammerArticle(XDocument xmlDoc)
     {
         var articles = xmlDoc.Descendants("item");
-        var latestArticleElement = articles.FirstOrDefault();
+        var latestArticleElement = articles.Where(x => x.Elements("category")
+        .Any(x => x.Value.Contains("40k") || x.Value.Contains("Warhammer 40k")))
+        .OrderByDescending(x => DateTimeHelper.GetPublicationDateFromElement(x))
+        .FirstOrDefault();
 
         if (latestArticleElement != null)
         {
+            string dateString = latestArticleElement.Element("pubDate")?.Value;
+            
             Article latestArticle = new Article
             {
                 Title = latestArticleElement.Element("title")?.Value,
                 Link = latestArticleElement.Element("link")?.Value,
-            };
+           };
+
+            if(DateTime.TryParse(dateString, out DateTime date))
+            {
+                latestArticle.PublicationDate = date;
+            }
+
+            return latestArticle;
         }
+
+        return null;
     }
 
 }
