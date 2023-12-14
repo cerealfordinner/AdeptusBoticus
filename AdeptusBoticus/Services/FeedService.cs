445f3973acf4ebfe0eeb121fe71d5c32283c08f9
Brandon Jones <cerealfordinnerx@gmail.com>
2023-12-14 16:09:30 -0500

    Add additional channelIds and categories

--- AdeptusBoticus/Services/FeedService.cs
+++ AdeptusBoticus/Services/FeedService.cs
@@ -13,7 +13,12 @@ public class FeedService
     private CommandService _commandService;
     private static System.Timers.Timer _articleCheckTimer;
     private string _discordApiKey;
-    private ulong _channelId;
+    private ulong _40kChannelId;
+    private string[] _40kCategories;
+    private ulong _horusHersyChannelId;
+    private string[] _horusHeresyCategories;
+    private ulong _fantasyChannelId;
+    private string[] _fantasyCategories;
     private string _feedUrl;
     public FeedService(IConfiguration configuration)
     {
@@ -21,11 +26,36 @@ public class FeedService
         // get all config values
         _feedUrl = _configuration["FeedUrl"];
         _discordApiKey = _configuration["DiscordApiKey"];
-        var ulongString = _configuration["ChannelId"];
-        if (ulong.TryParse(ulongString, out ulong channelId))
+
+        var ulong40k = configuration.GetSection("Warhammer40K:channelId").Value;
+        if (ulong.TryParse(ulong40k, out ulong channelId40k))
         {
-            _channelId = channelId;
+            _40kChannelId = channelId40k;
         }
+        _40kCategories = configuration.GetSection("Warhammer40K:categories")
+            .GetChildren()
+            .Select(x => x.Value)
+            .ToArray();
+
+        var ulongHH = configuration.GetSection("WarhammerHorusHeresy:channelId").Value;
+        if (ulong.TryParse(ulongHH, out ulong channelIdHH))
+        {
+            _horusHersyChannelId = channelIdHH;
+        }
+        _horusHeresyCategories = configuration.GetSection("Warhammer40K:categories")
+            .GetChildren()
+            .Select(x => x.Value)
+            .ToArray();
+
+        var ulongFantasy = configuration.GetSection("WarhammerFantasy:channelId").Value;
+        if (ulong.TryParse(ulongFantasy, out ulong channelIdFantasy))
+        {
+            _fantasyChannelId = channelIdFantasy;
+        }
+        _fantasyCategories = configuration.GetSection("Warhammer40K:categories")
+            .GetChildren()
+            .Select(x => x.Value)
+            .ToArray();
     }
 
     public async Task RunBotAsync()
@@ -36,7 +66,8 @@ public class FeedService
         await _client.LoginAsync(Discord.TokenType.Bot, _discordApiKey);
         await _client.StartAsync();
 
-        StartArticleCheckTimer();
+        // StartArticleCheckTimer();
+        await CheckForNewArticle();
 
         await Task.Delay(-1);
     }
@@ -61,15 +92,17 @@ public class FeedService
 
         // Get latest article that meets the category criteria
         var xmlDoc = await GetFeedAsync(_feedUrl);
-        Article latestArticle = GetLatestWarhammerArticle(xmlDoc);
+        Article latest40KArticle = GetLatestWarhammerArticle(xmlDoc, _40kCategories);
+        Article latestHorusHersyArticle = GetLatestWarhammerArticle(xmlDoc, _horusHeresyCategories);
+        Article latestFantasyArticle = GetLatestWarhammerArticle(xmlDoc, _fantasyCategories);
 
         // Get the dateTime of the last article posted
         DateTime latestPostedArticleDate = storageService.GetLastPostedArticleDate();
-        if (latestArticle != null && latestArticle.PublicationDate > latestPostedArticleDate)
+        if (latest40KArticle != null && latest40KArticle.PublicationDate > latestPostedArticleDate)
         {
-            await PostLatestArticle(_client, latestArticle.Link);
-            storageService.UpdateLastPostedArticleDate(latestArticle.PublicationDate);
-            Console.WriteLine(latestArticle.Title);
+            await PostLatestArticle(_client, latest40KArticle.Link);
+            storageService.UpdateLastPostedArticleDate(latest40KArticle.PublicationDate);
+            Console.WriteLine(latest40KArticle.Title);
         }
         else
         {
@@ -84,7 +117,7 @@ public class FeedService
 
     private async Task PostLatestArticle(DiscordSocketClient client, string articleLink)
     {
-        var channel = client.GetChannel(_channelId) as ISocketMessageChannel;
+        var channel = client.GetChannel(_40kChannelId) as ISocketMessageChannel;
         if (channel != null)
         {
             await channel.SendMessageAsync(articleLink);
@@ -99,7 +132,7 @@ public class FeedService
         return xmlDoc;
     }
 
-    public Article GetLatestWarhammerArticle(XDocument xmlDoc)
+    public Article GetLatestWarhammerArticle(XDocument xmlDoc, string[] categories)
     {
         var articles = xmlDoc.Descendants("item");
         var latestArticleElement = articles.Where(x => x.Elements("category")
