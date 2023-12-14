1cfaaafda55d5fd322e879ff0d552fd956a79956
Brandon Jones <cerealfordinnerx@gmail.com>
2023-12-14 18:57:41 -0500

    Add ArticleType enum

--- AdeptusBoticus/Models/Article.cs
+++ AdeptusBoticus/Models/Article.cs
@@ -2,6 +2,7 @@
 
 public class Article
 {
+    public ArticleType ArticleType { get; set; }
     public string Title { get; set; }
     public string Link { get; set; }
     public DateTime PublicationDate { get; set; }
---
+++ AdeptusBoticus/Models/ArticleType.cs
@@ -0,0 +1,9 @@
+namespace AdeptusBoticus
+{
+    public enum ArticleType
+    {
+        Warhammer40K,
+        WarhammerHorusHeresy,
+        WarhammerFantasy
+    }
+}
