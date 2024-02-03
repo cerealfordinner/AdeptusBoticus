78915e6b6f21aa124deb18aa542a324125ab79a2
Brandon Jones <cerealfordinnerx@gmail.com>
2024-02-03 15:41:26 -0500

    Remove unused method and update namespace

--- AdeptusBoticus/Helpers/StringExtensions.cs
+++ AdeptusBoticus/Helpers/StringExtensions.cs
@@ -1,19 +1,9 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace AdeptusBoticus;
+namespace AdeptusBoticus.Helpers;
 
 public static class StringExtensions
 {
-    public static int GetArticleIdFromlink(this string link)
-    {
-        var match = Regex.Match(link, @"(\d+)$");
-        if (match.Success && int.TryParse(match.Groups[1].Value, out int articleId))
-            {
-                return articleId;
-        }
-        return -1; // Invalid ID
-    }
-
     public static string StripHtmlTags(this string link)
     {
         return Regex.Replace(link, "<.*?>", string.Empty);
